fb10e2aaf6897185e4434f82e990b81472b57f47
Emerson Batista <emerson.dev21@gmail.com>
2024-07-08 14:47:50 -0300

    upload front desktop

--- assets/scripts/scipt.js
+++ assets/scripts/scipt.js
@@ -1,5 +1,17 @@
+function validarTexto(texto) {
+  // Expressão regular para permitir apenas letras minúsculas sem acentos
+  const regex = /^[a-z\s]*$/;
+  return regex.test(texto);
+}
+
 function criptografar() {
   let texto = document.getElementById('entrada').value;
+
+  if (!validarTexto(texto)) {
+    alert("Por favor, use apenas letras minúsculas sem acentos ou caracteres especiais.");
+    return;
+  }
+
   let textoCriptografado = texto
     .replace(/e/g, 'enter')
     .replace(/i/g, 'imes')
@@ -11,6 +23,12 @@ function criptografar() {
 
 function descriptografar() {
   let texto = document.getElementById('entrada').value;
+
+  if (!validarTexto(texto)) {
+    alert("Por favor, use apenas letras minúsculas sem acentos ou caracteres especiais.");
+    return;
+  }
+
   let textoDescriptografado = texto
     .replace(/enter/g, 'e')
     .replace(/imes/g, 'i')
@@ -18,4 +36,12 @@ function descriptografar() {
     .replace(/ober/g, 'o')
     .replace(/ufat/g, 'u');
   document.getElementById('resultado').value = textoDescriptografado;
+}
+
+function copiarTexto() {
+  navigator.clipboard.writeText(document.getElementById('resultado').value).then(() => {
+    alert("Texto copiado para a área de transferência.");
+  }, () => {
+    alert("Falha ao copiar o texto.");
+  });
 }
